8f59e9236e5319c8bc0685e9fc39b4387099a762
chenlei <chenleicpp@163.com>
2020-12-27 23:06:35 +0800

    Create quantumult-x-aqi-us.js

---
+++ Scripts/Surge/quantumult-x-aqi-us.js
@@ -0,0 +1,3 @@
+hostname = weather-data.apple.com
+
+https://weather-data.apple.com/v1/weather/[\w-_]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?include= url script-response-body https://raw.githubusercontent.com/chenleicpp/SS-Rule-Snippet/master/Scripts/Surge/iOS_Weather_AQI_Standard.js
